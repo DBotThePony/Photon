9c84324c7194521f9c31fc0b780060ecde6e92f1
John Internet <jonjon1234.github@gmail.com>
2019-12-13 14:56:03 +0000

    Add the rest of the functions.

--- lua/autorun/photon/sv_photon_meta.lua
+++ lua/autorun/photon/sv_photon_meta.lua
@@ -67,95 +67,143 @@ function ent:CAR_Running(val)
 	return self:PhotonBool("CAR_RUNNING", val)
 end
 
+-- Getter/Setter for indicator state.
+-- @bool[opt] val New indicator state.
+-- @treturn bool Current indicator state.
+function ent:CAR_Signal(val)
+	return self:PhotonInteger("CAR_BLINKER", val)
+end
+
+--- Wrapper to check a turn signal.
+-- @int check State to check against.
+-- @int[opt] val New state to set.
+function ent:CAR_TurnState(check, val)
+	if val ~= nil then
+		val = check
 	end
 
-	function ent:CAR_TurnLeft( val )
-		if not IsValid( self ) then return false end
-		if (val!=nil) then self:CAR_Signal( CAR_TURNING_LEFT ) end
-		return self:CAR_Signal() == CAR_TURNING_LEFT
+	return self:CAR_Signal(val) == check
+end
 
+--- Wrapper for checking left turn signals.
+-- @bool[opt] val New indicator state.
+-- @treturn bool If the vehicle has its left indicator enabled.
+function ent:CAR_TurnLeft(val)
+	return self:CAR_TurnState(CAR_BLINKER_LEFT, val)
+end
+
+--- Wrapper for checking right turn signals.
+-- @bool[opt] val New indicator state.
+-- @treturn bool If the vehicle has its right indicator enabled.
+function ent:CAR_TurnRight(val)
+	return self:CAR_TurnState(CAR_BLINKER_RIGHT, val)
+end
+
+--- Wrapper for checking hazard lights.
+-- @bool[opt] val New hazard state.
+-- @treturn bool If the vehicle has its hazards enabled.
+function ent:CAR_Hazards(val)
+	return self:CAR_TurnState(CAR_BLINKER_HAZARD, val)
+end
+
+--- Disable all car signals.
+function ent:CAR_StopSignals()
+	return self:CAR_TurnState(CAR_BLINKER_NONE)
+end
+
+--- Check if a given entity is currently breaking.
+function ent:IsBraking()
+	if not IsValid(self) then return false end
+	if self:IsReversing() then return false end
+
+	-- They're holding the handbreak.
+	local driver = self:GetDriver()
+	if driver:KeyDown(IN_JUMP) then
+		return true
 	end
 
-	function ent:CAR_TurnRight( val )
-		if not IsValid( self ) then return false end
-		if (val!=nil) then self:CAR_Signal( CAR_TURNING_RIGHT ) end
-		return self:CAR_Signal() == CAR_TURNING_RIGHT
-
-	end
-
-	function ent:CAR_Hazards( val )
-		if not IsValid( self ) then return false end
-		if (val!=nil) then self:CAR_Signal( CAR_HAZARD ) end
-		return self:CAR_Signal() == CAR_HAZARD
-
-	end
-
-	function ent:CAR_StopSignals()
-		if not IsValid( self ) then return false end
-		self:CAR_Signal( 0 )
-	end
-
-	function ent:IsBraking( )
-		if not IsValid( self ) then return false end
-		if self:IsReversing() then return false end
-		local speed = self:GetPhysicsObject():GetVelocity():Length()
-		if not self.LastSpeed then self.LastSpeed = speed return false end
-		if (self.LastSpeed - speed) >= 0 and ( self:GetDriver():KeyDown( IN_BACK ) and not self:IsReversing() ) or ( self:GetDriver():KeyDown( IN_BACK) and self:Photon_AdjustedSpeed() < 2 ) or self:GetDriver():KeyDown( IN_JUMP ) then
-			return true
-		end
+	-- If they're not holding the handbreak or the regular break, then we're not breaking.
+	if not driver:KeyDown(IN_BACK) then
 		return false
 	end
 
-	function ent:IsReversing()
-		if not IsValid( self ) then return false end
-		if self:GetDriver() and self:GetDriver():IsValid() and self:GetDriver():IsPlayer() then
-			local ply = self:GetDriver()
-			local velocity = self:GetPhysicsObject():GetVelocity()
-			velocity:Normalize()
-			local direction = self:GetForward()
-			local sum = Vector()
-			sum.x = velocity.x * direction.x
-			sum.y = velocity.y * direction.y
-			if not (sum.x >= 0 and sum.y >= 0) then
-				if (ply:KeyDown( IN_BACK ) and (ent:Photon_AdjustedSpeed() > 10)) and not ply:KeyDown( IN_JUMP ) then return true end
-			end
-		end
+	-- We haven't yet recorded a speed, so it's better to be safe and not.
+	local speed = self:GetPhysicsObject():GetVelocity():Length()
+	if not self.LastSpeed then self.LastSpeed = speed return false end
+
+	local slowing = (self.LastSpeed - speed) >= 0
+	if slowing then return true end
+
+	if self:Photon_AdjustedSpeed() < 2 then return true end
+
+	return false
+end
+
+-- Check if we're currently reversing.
+function ent:IsReversing()
+	if not IsValid(self) then return false end
+
+	local ply = self:GetDriver()
+	if not IsValid(ply) then return false end
+	if not ply:IsPlayer() then return false end
+
+	local vel = self:GetPhysicsObject():GetVelocity()
+	vel:Normalize()
+
+	-- Check we're actually moving backwards.
+	local sum = vel * self:GetForward()
+	if sum.x < 0 or sum.y < 0 then
 		return false
 	end
 
-	function ent:GetPhotonLEStayOn()
-		if GetConVar("photon_emv_stayon"):GetBool() then return true end
-		return self:GetNW2Bool("PhotonLEStayOn", false)
-	end
-	function ent:SetPhotonLEStayOn(val)
-		return self:SetNW2Bool("PhotonLEStayOn", val)
+	if not ply:KeyDown(IN_BACK) then
+		return false
 	end
 
-	function ent:Photon_WheelEnabled()
-		return istable( Photon.Vehicles.WheelPositions[ self.VehicleName ] ) and istable( Photon.Vehicles.WheelOptions[ self.VehicleName ] )
+	if self:Photon_AdjustedSpeed() <= 5 then
+		return false
 	end
 
-	function ent:Photon_PlayerSetWheelIndex( val )
-		if not self:Photon_WheelEnabled() then return false end
-		local max = #Photon.Vehicles.WheelOptions[ self.VehicleName ]
-		if val > max then val = 1 end
-		self:Photon_SetWheelIndex( val )
+	if ply:KeyDown(IN_JUMP) then return false end
+	return true
+end
+
+local stayon = GetConVar("photon_emv_stayon")
+function ent:GetPhotonLEStayOn()
+	return stayon:GetBool() or self:GetNW2Bool("PhotonLEStayOn", false)
+end
+
+function ent:SetPhotonLEStayOn(val)
+	return self:SetNW2Bool("PhotonLEStayOn", val)
+end
+
+function ent:Photon_WheelEnabled()
+	return istable( Photon.Vehicles.WheelPositions[ self.VehicleName ] ) and istable( Photon.Vehicles.WheelOptions[ self.VehicleName ] )
+end
+
+function ent:Photon_PlayerSetWheelIndex( val )
+	if not self:Photon_WheelEnabled() then return false end
+	local max = #Photon.Vehicles.WheelOptions[ self.VehicleName ]
+	if val > max then val = 1 end
+	self:Photon_SetWheelIndex( val )
+end
+
+function ent:Photon_SetWheelIndex( val )
+	if not IsValid( self ) then return 0 end
+	if (val ~= nil) then self:SetNW2Int( "PhotonLE.CAR_WHEEL_OPTION", val ) end
+	return self:GetNW2Int( "PhotonLE.CAR_WHEEL_OPTION" )
+end
+
+function Photon:SetupCar(car, index)
+	car:CAR_Headlights(false)
+	car:CAR_Braking(false)
+	car:CAR_Running(false)
+	car:CAR_Reversing(false)
+	car:CAR_StopSignals()
+
+	if car:Photon_WheelEnabled() then
+		car:Photon_SetWheelIndex(1)
 	end
 
-	function ent:Photon_SetWheelIndex( val )
-		if not IsValid( self ) then return 0 end
-		if (val!=nil) then self:SetNW2Int( "PhotonLE.CAR_WHEEL_OPTION", val ) end
-		return self:GetNW2Int( "PhotonLE.CAR_WHEEL_OPTION" )
-	end
-
-	ent:CAR_Headlights( false )
-	ent:CAR_Braking( false )
-	ent:CAR_Running( false )
-	ent:CAR_Reversing( false )
-	ent:CAR_StopSignals()
-	if ent:Photon_WheelEnabled() then ent:Photon_SetWheelIndex( 1 ) end
-
-	-- ent:SetNWString( "PhotonVehicle", index )
-	ent:SetNW2Bool( "PhotonLE.CAR_HAS_PHOTON", true )
-
+	car:PhotonBool("CAR_HAS_PHOTON", true)
 end
